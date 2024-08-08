fb90763a78dcec85bb4aeb078455a08145c19e9f
Emma Huang <huiwenhuang1998@gmail.com>
2024-08-08 16:16:12 +0800

    update home page

--- src/components/ShopFooter.vue
+++ src/components/ShopFooter.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="col-md-12 p-0 mt-4 position-relative">
-    <div style="height: 500px; background-size: cover; background-position: center"
+  <div class="col-md-12 p-0 position-relative">
+    <div style="height: 400px; background-size: cover; background-position: center"
         :style="{backgroundImage: `url(${require('@/assets/images/footer-bn.jpg')})`}">
-      <h1 class="text-center text-warning fw-bold position-absolute top-50 start-0">歡迎訂閱！</h1>
+      <div class="position-relative top-50 start-0 w-50 ms-5">
+        <div class="m-2 fs-5">歡迎訂閱我們，隨時獲得最新優惠！
+          <div class="input-group mt-3 position-absolute top-75 start-0 w-75">
+            <input type="text" class="form-control" placeholder="輸入信箱立即訂閱" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">訂閱</button>
+          </div>
+        </div>
+      </div>
     </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-5">
+      <div class="container-fluid">
+        <div class="navbar-brand text-warning fw-bold mx-auto" href="#">Hokii Poke
+          <span class="navbar-text mx-auto fs-6 fw-light px-3">© 2024. All Rights Reserved.</span>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item mx-2 fw-bold">
+                <router-link class="nav-link" to="/">首頁</router-link>
+            </li>
+            <li class="nav-item mx-2 fw-bold">
+              <router-link class="nav-link" to="/about">關於 Hokii</router-link>
+            </li>
+            <li class="nav-item mx-2 fw-bold">
+              <router-link class="nav-link" to="/products">全店商品</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
--- src/views/ShopHomePage.vue
+++ src/views/ShopHomePage.vue
@@ -3,75 +3,94 @@
     <div class="container-fluid">
       <div class="row p-0">
         <!-- Banner -->
-        <div class="col-md-12 p-0 mb-4 position-relative">
+        <div class="col-md-12 p-0 mb-md-4 mb-3 position-relative animate__animated animate__fadeInUp">
           <div style="height: 800px; background-size: cover; background-position: center"
               :style="{backgroundImage: `url(${require('@/assets/images/banner-1.jpg')})`}">
-            <h1 class="text-center text-warning fw-bold position-absolute top-50 start-50 translate-middle animate__animated animate__fadeIn">Welcome to Hokii poke!</h1>
+            <h1 class="text-center text-warning fw-bold position-absolute top-50 start-50 translate-middle animate__animated animate__rollIn animate__delay-1s">Welcome to Hokii poke!</h1>
           </div>
         </div>
         <!-- 三大主題 -->
-        <div class="row justify-content-center mt-4">
-          <div class="col-md-4 animate__animated animate__slideInLeft p-0">
-            <img src="../assets/images/vagetable.jpg" style="width: 472px;" alt="">
-          </div>
-          <div class="col-md-4 animate__animated animate__slideInUp p-5">
-            <h5 class="fw-bold mb-4">蔬食健康無負擔</h5>
-            <div>蔬食不僅能降低心血管疾病的風險，還能促進消化系統健康，增加能量和活力。
-              <br>
-              Hokii 的夏威夷飯以新鮮蔬果為主，提供均衡的營養，同時減少對環境的負擔。
-              <br>
-              無論是素食者還是尋求健康飲食的人，這都是您的理想選擇。
+        <div class="container-fluid mb-4">
+          <div class="row justify-content-center mt-md-4">
+            <div class="col-md-4 p-0" data-aos="fade-right">
+              <img src="../assets/images/vagetable.jpg" class="img-fluid" style="max-width: 100%; height: auto;" alt="">
+            </div>
+            <div class="col-md-4 p-5" data-aos="fade-up">
+              <h5 class="fw-bold mb-4">蔬食健康無負擔</h5>
+              <div>蔬食不僅能降低心血管疾病的風險，還能促進消化系統健康，增加能量和活力。
+                <br>
+                Hokii 的夏威夷飯以新鮮蔬果為主，提供均衡的營養，同時減少對環境的負擔。
+                <br>
+                無論是素食者還是尋求健康飲食的人，這都是您的理想選擇。
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-md-4 animate__animated animate__slideInRight p-5">
-            <h5 class="fw-bold mb-4 pt-4">支持在地小農</h5>
-            <p class="my-auto">Hokii 堅持採購本地農場的新鮮食材，支持在地小農的生計與可持續發展。
-              <br>
-              每一碗夏威夷飯都飽含對社區的愛與支持，讓您在享受美味的同時，也能為環保和當地經濟盡一份力。
-            </p>
+          <div class="row flex-md-row-reverse justify-content-center">
+            <div class="col-md-4 p-0" data-aos="fade-left">
+              <img src="../assets/images/farm.jpg" class="img-fluid" style="max-width: 100%; height: auto;" alt="">
+            </div>
+            <div class="col-md-4 p-5" data-aos="fade-up">
+              <h5 class="fw-bold mb-4 pt-4">支持在地小農</h5>
+              <p class="my-auto">Hokii 堅持採購本地農場的新鮮食材，支持在地小農的生計與可持續發展。
+                <br>
+                每一碗夏威夷飯都飽含對社區的愛與支持，讓您在享受美味的同時，也能為環保和當地經濟盡一份力。
+              </p>
+            </div>
           </div>
-          <div class="col-md-4 animate__animated animate__slideInUp p-0">
-            <img src="../assets/images/farm.jpg" style="width: 472px;" alt="">
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-md-4 animate__animated animate__slideInLeft p-0">
-            <img src="../assets/images/market.jpg" style="width: 472px;" alt="">
-          </div>
-          <div class="col-md-4 animate__animated animate__slideInUp p-5">
-            <h5 class="fw-bold mb-4 pt-4">產地新鮮直送</h5>
-            <p>我們的食材直接從產地新鮮直送，確保每一口都充滿自然的鮮美滋味。
-              <br>
-              無論是多汁的水果還是爽脆的蔬菜，都是每日現採，保證最佳的品質與口感。
-              <br>
-              享受健康美味，就從 Hokii 的夏威夷飯開始。
-            </p>
+          <div class="row justify-content-center">
+            <div class="col-md-4 p-0" data-aos="fade-right">
+              <img src="../assets/images/market.jpg" class="img-fluid" style="max-width: 100%; height: auto;" alt="">
+            </div>
+            <div class="col-md-4 p-5" data-aos="fade-up">
+              <h5 class="fw-bold mb-4 pt-4">產地新鮮直送</h5>
+              <p>我們的食材直接從產地新鮮直送，確保每一口都充滿自然的鮮美滋味。
+                <br>
+                無論是多汁的水果還是爽脆的蔬菜，都是每日現採，保證最佳的品質與口感。
+                <br>
+                享受健康美味，就從 Hokii 的夏威夷飯開始。
+              </p>
+            </div>
           </div>
         </div>
         <!-- 商品資訊 -->
-        <div class="row justify-content-center mt-4 bg-light py-5">
-          <div class="col-md-8 d-flex justify-content-center">
-            <div class="card col-md-3 me-4 border-0 bg-light">
-              <img src="../assets/images/poke-3.jpg" class="card-img-top rounded-0" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Hokii 經典鮪魚飯</h5>
-                <p class="card-text">NT$ 189</p>
+        <div class="container-fluid bg-light">
+          <div class="row justify-content-center mt-4 bg-light my-md-5 my-3" data-aos="zoom-in">
+            <div class="col-md-10 d-flex flex-wrap justify-content-around p-md-0 p-4 py-md-5">
+              <div class="card col-md-3 border-0 mb-4 shadow" data-aos="zoom-in">
+                <img src="../assets/images/poke-3.jpg" alt="">
+                <div class="card-body px-4 bg-light">
+                  <h5 class="card-title fw-bold">Hokii 經典鮪魚飯</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <p class="card-text m-0">NT$ 189</p>
+                    <button class="btn" @click="addCart('-O0x2-SXPpPn9m0Wi0i_')">
+                      <i class="bi bi-cart4"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="card col-md-3 me-4 border-0 bg-light">
-              <img src="../assets/images/poke-4.jpg" class="card-img-top rounded-0" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Hokii 酪梨鮪魚飯</h5>
-                <p class="card-text">NT$ 179</p>
+              <div class="card col-md-3 border-0 mb-4 shadow">
+                <img src="../assets/images/poke-4.jpg" alt="">
+                <div class="card-body px-4 bg-light" data-aos="zoom-in">
+                  <h5 class="card-title fw-bold">Hokii 酪梨鮪魚飯</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <p class="card-text m-0">NT$ 179</p>
+                    <button class="btn" @click="addCart('-O10pJDEYr17j6dkd9e5')">
+                      <i class="bi bi-cart4"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="card col-md-3 me-4 border-0 bg-light">
-              <img src="../assets/images/poke-2.jpg" class="card-img-top rounded-0" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Hokii 優格沙拉</h5>
-                <p class="card-text">NT$ 179</p>
+              <div class="card col-md-3 border-0 mb-md-4 shadow">
+                <img src="../assets/images/poke-2.jpg" alt="">
+                <div class="card-body px-4 bg-light" data-aos="zoom-in">
+                  <h5 class="card-title fw-bold">Hokii 優格沙拉</h5>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <p class="card-text m-0">NT$ 179</p>
+                    <button class="btn" @click="addCart('-O1GAAIjQ6TNInvRPiDg')">
+                      <i class="bi bi-cart4"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -99,7 +118,8 @@ export default {
   },
   methods: {
     // 取得購物車數量
-    ...mapActions(cartStore, ['getCart'])
+    ...mapActions(cartStore, ['getCart']),
+    ...mapActions(productStore, ['addCart'])
   },
   created () {
     this.getCart()
